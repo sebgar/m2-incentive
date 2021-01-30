52b691de42f2eaf622615a9ae1cedab7064057d8
sgarrouste <sgarrouste@datasolution.fr>
2021-01-30 14:37:05 +0100

    composer.json + readme

--- Model/ResourceModel/Push.php
+++ Model/ResourceModel/Push.php
@@ -6,6 +6,7 @@ use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 use Magento\Framework\Model\ResourceModel\Db\Context;
+use Magento\Framework\Stdlib\DateTime\DateTime;
 use Magento\Store\Model\StoreManagerInterface;
 use Sga\Incentive\Api\Data\PushInterface as ModelInterface;
 
@@ -14,17 +15,20 @@ class Push extends AbstractDb
     protected $_storeManager;
     protected $_entityManager;
     protected $_metadataPool;
+    protected $_dateTime;
 
     public function __construct(
         Context $context,
         StoreManagerInterface $storeManager,
         EntityManager $entityManager,
         MetadataPool $metadataPool,
+        DateTime $dateTime,
         $connectionName = null
     ) {
         $this->_storeManager = $storeManager;
         $this->_entityManager = $entityManager;
         $this->_metadataPool = $metadataPool;
+        $this->_dateTime = $dateTime;
 
         parent::__construct($context, $connectionName);
     }
@@ -50,6 +54,11 @@ class Push extends AbstractDb
 
     public function save(AbstractModel $object)
     {
+        if ($object->isObjectNew()) {
+            $object->setCreatedAt($this->_dateTime->gmtDate());
+        }
+        $object->setUpdatedAt($this->_dateTime->gmtDate());
+
         $this->packData($object);
         $this->_entityManager->save($object);
         return $this;
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # m2-incentive
+
+Ajout de bloc CMS dans les listing produits 
+
+## Installation
+
+Via composer
+
+```bash
+composer require sebgar/m2-incentive
+```
